a9d55f4d081049166b89c7cddbd124e2590d34f3
Irghanchi <irghanchi@gmail.com>
2021-08-10 22:15:46 +0530

    changes in bouncing_ball.py

--- examples/bouncing_ball.py
+++ examples/bouncing_ball.py
@@ -1,21 +1,22 @@
 from utils import  Color
 from anim import Anim
 from shapes import Circle
+import numpy as np
 
 
-posy = 3
-vely = 0
+pos = np.array([0,3])
+vel = np.array([0,0])
 g = -1
 gor = Circle(radius= 2,color = Color.red)
 
 def anim_func(dt):
-    global posy,vely,g,gor
-    gor.move_to(0,posy)
-    vely += g*dt
-    posy += vely*dt
-    if posy < -3:
-        posy = -3
-        vely = -vely
+    global pos,vel,g,gor
+    gor.move_to(0,pos)
+    vel += g*dt
+    pos += vel*dt
+    if pos[1] < -3:
+        pos[1] = -2.9
+        vel[1] = -vel[1]
 ani = Anim()
 ani.make_animation(anim_func)
 
